32ea0f1ae9994f7d05464c41f9844f66e4afd9da
Anna Slobodian <slobodian.ann@gmail.com>
2025-10-03 22:53:47 +0300

    feat: update footer

--- my-app/src/components/footer/footer.js
+++ my-app/src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import './footer.css';
+import oferta from '../../img/oferta.docx';
 
 const FooterContainer = () => {
   return (
@@ -11,6 +12,23 @@ const FooterContainer = () => {
         <a className="btn btn-catalog" href="#catalog">
           voir le catalogue
         </a>
+        <div>
+          <a className="oferta-link" href={oferta} target="_blank" rel="noopener noreferrer">
+            Offre
+          </a>
+          <div>Pour toute demande de retour, veuillez nous contacter par e-mail: farvest23@gmail.com </div>
+          <div className="oferta-info">
+            Nom complet : Boris Alexandrovitch Afanasyev <br />
+            NIF : 575109662909
+            <br />
+            OGRN/OGRNIP : 324570000023805
+            <br />
+            Numéro de téléphone : +7 915 129-42-72
+            <br />
+            E-mail de contact : farvest23@gmail.com
+            <br />
+          </div>
+        </div>
       </div>
     </div>
   );
